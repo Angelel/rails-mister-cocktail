bca48189f0f5c7f9c50a18cd0c270716f61eee4a
Angele Lenglemetz <angele.lenglemetz@gmail.com>
2018-02-22 16:16:19 +0000

    Made a cocktail website

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+before_action :set_doses, only: [:show]
+before_action :set_cocktail, only: [:new, :create]
+
+  # def index
+  #   @doses = cocktail.dose.all
+  # end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+  def set_dose
+    @dose = dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# Prefix Verb   URI Pattern                   Controller#Action
+#     cocktails GET    /cocktails(.:format)          cocktails#index
+#               POST   /cocktails(.:format)          cocktails#create
+#  new_cocktail GET    /cocktails/new(.:format)      cocktails#new
+# edit_cocktail GET    /cocktails/:id/edit(.:format) cocktails#edit
+#      cocktail GET    /cocktails/:id(.:format)      cocktails#show
+#               PATCH  /cocktails/:id(.:format)      cocktails#update
+#               PUT    /cocktails/:id(.:format)      cocktails#update
+#               DELETE /cocktails/:id(.:format)      cocktails#destroy
+resources :cocktails do
+
+  resources :doses, only: [:new, :create, :show, :destroy]
+  end
+  resources :doses, only: [:destroy]
 end
